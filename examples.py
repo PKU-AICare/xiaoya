de3da8978ed46914440ce9263fe6fef868a2a5a8
akai <19241078@buaa.edu.cn>
2023-07-03 20:47:50 +0800

    chore: add examples for testing

--- examples.py
+++ examples.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 
 import xiaoya
-from xiaoya import pyehr  # training pipeline, models, etc
 from xiaoya import data  # data handler, data loaders, etc
 from xiaoya import pipeline # training and predict pipeline
 
@@ -24,10 +22,10 @@ for info in data_handler.analyze_dataset():
 # execute the preprocessing pipeline.
 data_handler.execute()
 
-demo_dim = len(features['events_features']) + 1
-lab_dim =  len(features['labtest_features']) - 1
-pipeline = pipeline.Pipeline(dataset=data_handler.merged_df, model='MLP', demographic_dim=demo_dim, labtest_dim=lab_dim)
+demo_dim = len(features['events_features']) + 1     # +1 for the age feature
+lab_dim =  len(features['labtest_features']) - 1    # -1 for the age feature
+pl = pipeline.Pipeline(model='GRU', demographic_dim=demo_dim, labtest_dim=lab_dim)
 
 # execute the training pipeline, returns the performance of the model.
-performance = pipeline.execute()
+performance = pl.execute()
 print(performance)
