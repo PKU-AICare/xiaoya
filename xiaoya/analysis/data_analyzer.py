3b4ec1007958db4451f7e080c5ac5e6aeeb3aef5
Wei Xia <1114891937@qq.com>
2023-07-12 12:50:20 +0800

    feat:Add get dimension reduction coordinate function

--- xiaoya/analysis/data_analyzer.py
+++ xiaoya/analysis/data_analyzer.py
@@ -1,8 +1,13 @@
 import torch
 import lightning as L
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from typing import List
 
 from xiaoya.pyehr.pipelines import DlPipeline
 from xiaoya.pipeline import Pipeline
+
 
 class DataAnalyzer:
     """
@@ -31,5 +36,33 @@ class DataAnalyzer:
         pipeline = DlPipeline(config)
         pipeline = pipeline.load_from_checkpoint(self.model_path)
         y_hat, embedding, scores = pipeline.predict_step(x)
-
         return scores
+
+    def data_dimension_reduction(df,method,dimension,target)-> List:
+        if method == "PCA":  # 判断降维类别
+            reduction_model = PCA().fit_transform(df)
+        elif method == "TSNE":
+            reduction_model = TSNE(n_components=dimension).fit_transform(df)
+        df_target = df[target]
+        if dimension == 2:  # 判断降维维度
+            print(df_target.shape, reduction_model.shape)
+            df_subset = pd.DataFrame(
+                {
+                    "2d-one": reduction_model[:, 0],
+                    "2d-two": reduction_model[:, 1],
+                    "target": df_target,
+                }
+            )
+            return [df_subset["2d-one"].tolist(), df_subset["2d-two"].tolist(), df_subset["target"].tolist()]
+        elif dimension == 3:
+            df_subset = pd.DataFrame(
+                {
+                    "3d-one": reduction_model[:, 0],
+                    "3d-two": reduction_model[:, 1],
+                    "3d-three": reduction_model[:, 2],
+                    "target": df_target,
+                }
+            )
+            return [df_subset["3d-one"].tolist(), df_subset["3d-two"].tolist(), df_subset["3d-three"].tolist(), df_subset["target"].tolist()]
+        res = {"message": "reduction fail"}
+        return res
