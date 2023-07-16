5b0152a448f5f7948ae204dcc9d04e7de59b601c
Wei Xia <1114891937@qq.com>
2023-07-16 22:43:10 +0800

    merge

--- xiaoya/analysis/data_analyzer.py
+++ xiaoya/analysis/data_analyzer.py
@@ -73,5 +73,3 @@ class DataAnalyzer:
                 }
             )
             return list(zip([df_subset["3d-one"].tolist(), df_subset["3d-two"].tolist(), df_subset["3d-three"].tolist(), df_subset["target"].tolist()]))
-        res = {"message": "reduction fail"}
-        return res
