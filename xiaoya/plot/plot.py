66117f2c611ceac5bcb3e2e6c8321bbc5ca02c56
Fieldry <19241078@buaa.edu.cn>
2023-10-19 10:55:14 +0800

    refactor: tools function

--- xiaoya/plot/plot.py
+++ xiaoya/plot/plot.py
@@ -5,175 +5,165 @@ import pandas as pd
 import seaborn as sns
 
 
-class Plot:
+def plot_vis_dataset(
+        data: List,
+        save_path: str,
+    ) -> None:
     """
-    Plot tools.
+    Plot the distribution of the dataset.
 
+    Args:
+        data: List.
+            List of patients' data.
+        save_path: str.
+            Path to save the plot.
     """
 
-    def __init__(self) -> None:
-        pass
+    for feature in data:
+        plt.hist(feature['value'], bins=20, edgecolor='black')
+        plt.title(f'{feature["name"]}')
+        plt.xlabel('Value')
+        plt.ylabel('Count')
+        plt.savefig(f'{save_path}/{feature["name"]}_hist.png')
 
-    def plot_vis_dataset(
-            self,
-            data: List,
-            save_path: str,
-        ) -> None:
-        """
-        Plot the distribution of the dataset.
 
-        Args:
-            data: List.
-                List of patients' data.
-            save_path: str.
-                Path to save the plot.
-        """
+def plot_feature_importance(
+        data: List,
+        save_path: str,
+        feature_num: int=10,
+        file_name: str='feature_importance',
+    ) -> None:
+    """
+    Plot the feature importance as a bar chart.
 
-        for feature in data:
-            plt.hist(feature['value'], bins=20, edgecolor='black')
-            plt.title(f'{feature["name"]}')
-            plt.xlabel('Value')
-            plt.ylabel('Count')
-            plt.savefig(f'{save_path}/{feature["name"]}_hist.png')
+    Args:
+        data: List.
+            List of patients' data.
+        save_path: str.
+            Path to save the plot.
+        feature_num: int.
+            Number of features to plot, default 10.
+        file_name: str.
+            File name of the plot.
+    """
 
-    def plot_feature_importance(
-            self,
-            data: List,
-            save_path: str,
-            feature_num: int=10,
-            file_name: str='feature_importance',
-        ) -> None:
-        """
-        Plot the feature importance as a bar chart.
+    importance = dict(zip(data['name'], data['value']))
+    importance = sorted(importance.items(), key=lambda x: x[1], reverse=True)
+    names = [item[0] for item in importance[:feature_num]]
+    values = [item[1] for item in importance[:feature_num]]
 
-        Args:
-            data: List.
-                List of patients' data.
-            save_path: str.
-                Path to save the plot.
-            feature_num: int.
-                Number of features to plot, default 10.
-            file_name: str.
-                File name of the plot.
-        """
+    plt.figure(figsize=(12, 6))
+    plt.barh(names, values, color='blue', alpha=0.75)
+    plt.xlabel('Importance Index')
+    plt.title('Feature Importance')
+    plt.xlim(min(values) - 0.001, 1)  # Adjust the x-axis range for better visualization
+    plt.gca().invert_yaxis()  # Invert y-axis for top-down display
+    plt.tight_layout()
+    plt.savefig(f'{save_path}/{file_name}.png')
 
-        importance = dict(zip(data['name'], data['value']))
-        importance = sorted(importance.items(), key=lambda x: x[1], reverse=True)
-        names = [item[0] for item in importance[:feature_num]]
-        values = [item[1] for item in importance[:feature_num]]
 
-        plt.figure(figsize=(12, 6))
-        plt.barh(names, values, color='blue', alpha=0.75)
-        plt.xlabel('Importance Index')
-        plt.title('Feature Importance')
-        plt.xlim(min(values) - 0.001, 1)  # Adjust the x-axis range for better visualization
-        plt.gca().invert_yaxis()  # Invert y-axis for top-down display
-        plt.tight_layout()
-        plt.savefig(f'{save_path}/{file_name}.png')
-        
-    def plot_risk_curve(
-            self,
-            data: Dict,
-            save_path: str,
-            feature_num: int=3,
-            file_name: str='risk_curve',
-        ) -> None:
-        """
-        Plot the risk curve of a patient.
+def plot_risk_curve(
+        data: Dict,
+        save_path: str,
+        feature_num: int=3,
+        file_name: str='risk_curve',
+    ) -> None:
+    """
+    Plot the risk curve of a patient.
 
-        Args:
-            data: Dict.
-                Data to plot.
-            save_path: str.
-                Path to save the plot.
-            feature_num: int.
-                Number of features to plot, default 3.
-            file_name: str.
-                File name of the plot.
-        """
+    Args:
+        data: Dict.
+            Data to plot.
+        save_path: str.
+            Path to save the plot.
+        feature_num: int.
+            Number of features to plot, default 3.
+        file_name: str.
+            File name of the plot.
+    """
 
-        detail = data['detail']
-        time = data['time']
-        risk_index = data['time_step_importance']
-        feature_importance = data['feature_importance']
-        sorted_importance = sorted(feature_importance)[:feature_num]
-        index = [feature_importance.index(item) for item in sorted_importance]
+    detail = data['detail']
+    time = data['time']
+    risk_index = data['time_step_importance']
+    feature_importance = data['feature_importance']
+    sorted_importance = sorted(feature_importance)[:feature_num]
+    index = [feature_importance.index(item) for item in sorted_importance]
 
-        x = list(range(len(time)))
-        x_label = time
-        ys = [risk_index]
-        y_labels = ['Risk Index']
-        for i in index:
-            ys.append(detail[i]['value'])
-            y_labels.append(detail[i]['name'])
-        colors = sns.color_palette("hls", len(ys))
+    x = list(range(len(time)))
+    x_label = time
+    ys = [risk_index]
+    y_labels = ['Risk Index']
+    for i in index:
+        ys.append(detail[i]['value'])
+        y_labels.append(detail[i]['name'])
+    colors = sns.color_palette("hls", len(ys))
 
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.set_xlabel('Date', fontsize=14)
-        ax.set_xticks(ticks=x, labels=x_label, rotation=45, fontsize=14)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.set_xlabel('Date', fontsize=14)
+    ax.set_xticks(ticks=x, labels=x_label, rotation=45, fontsize=14)
 
-        twins = [ax]
-        for i in range(len(ys) - 1):
-            twins.append(twins[-1].twinx())
-        pad = 0
-        for i, twin in enumerate(twins):
-            twin.plot(x, ys[i], 'o-', color=colors[i], label=y_labels[i])
-            twin.set_ylabel(y_labels[i], fontsize=14)
-            pos = 'right' if i > 0 else 'left'
-            twin.yaxis.set_ticks_position(pos)
-            twin.yaxis.set_label_position(pos)
-            twin.yaxis.label.set_color(colors[i])
-            if pos == 'right':
-                twin.spines[pos].set_position(('outward', pad))
-                pad += 60
-            else:
-                twin.set_ylim(0.2, 1)
-                twin.fill_between(x, ys[i], color=colors[i], alpha=0.1)
-        plt.title('Health Metrics Over Time')
-        plt.savefig(f'{save_path}/{file_name}.png')
+    twins = [ax]
+    for i in range(len(ys) - 1):
+        twins.append(twins[-1].twinx())
+    pad = 0
+    for i, twin in enumerate(twins):
+        twin.plot(x, ys[i], 'o-', color=colors[i], label=y_labels[i])
+        twin.set_ylabel(y_labels[i], fontsize=14)
+        pos = 'right' if i > 0 else 'left'
+        twin.yaxis.set_ticks_position(pos)
+        twin.yaxis.set_label_position(pos)
+        twin.yaxis.label.set_color(colors[i])
+        if pos == 'right':
+            twin.spines[pos].set_position(('outward', pad))
+            pad += 60
+        else:
+            twin.set_ylim(0.2, 1)
+            twin.fill_between(x, ys[i], color=colors[i], alpha=0.1)
+    plt.title('Health Metrics Over Time')
+    plt.savefig(f'{save_path}/{file_name}.png')
 
-    def plot_patient_embedding(
-            self,
-            data: List,
-            save_path: str,
-            dimension: int = 2,    
-        ) -> None:
-        """
-        Plot patients' embeddings in 2D or 3D space.
 
-        Args:
-            data: List.
-                List of patients' embeddings.
-            save_path: str.
-                Path to save the plot.
-            dimension: int.
-                Dimension of the plot. Must be 2 or 3.
-        """
+def plot_patient_embedding(
+        data: List,
+        save_path: str,
+        dimension: int = 2,    
+    ) -> None:
+    """
+    Plot patients' embeddings in 2D or 3D space.
 
-        assert dimension in [2, 3], "dimension must be 2 or 3"
+    Args:
+        data: List.
+            List of patients' embeddings.
+        save_path: str.
+            Path to save the plot.
+        dimension: int.
+            Dimension of the plot. Must be 2 or 3.
+    """
 
-        plt.figure(figsize=(6, 6))
-        for patient in data:   
-            if dimension == 2: 
-                df_subset = pd.DataFrame(data=patient[0]['value'], columns=['2d-one', '2d-two', 'target'])
-                sns.scatterplot(
-                    x="2d-one",
-                    y="2d-two",
-                    hue="target",
-                    palette=sns.color_palette("coolwarm", as_cmap=True),
-                    data=df_subset,
-                    legend=False,
-                    alpha=0.3,
-                )
-            elif dimension == 3:
-                df_subset = pd.DataFrame(data=patient[0]['value'], columns=['3d-one', '3d-two', '3d-three', 'target'])
-                sns.scatterplot(
-                    x="3d-one",
-                    y="3d-two",
-                    hue="target",
-                    palette=sns.color_palette("coolwarm", as_cmap=True),
-                    data=df_subset,
-                    legend=False,
-                    alpha=0.3,
-                )
-        plt.savefig(save_path)
+    assert dimension in [2, 3], "dimension must be 2 or 3"
+
+    plt.figure(figsize=(6, 6))
+    for patient in data:   
+        if dimension == 2: 
+            df_subset = pd.DataFrame(data=patient[0]['value'], columns=['2d-one', '2d-two', 'target'])
+            sns.scatterplot(
+                x="2d-one",
+                y="2d-two",
+                hue="target",
+                palette=sns.color_palette("coolwarm", as_cmap=True),
+                data=df_subset,
+                legend=False,
+                alpha=0.3,
+            )
+        elif dimension == 3:
+            df_subset = pd.DataFrame(data=patient[0]['value'], columns=['3d-one', '3d-two', '3d-three', 'target'])
+            sns.scatterplot(
+                x="3d-one",
+                y="3d-two",
+                hue="target",
+                palette=sns.color_palette("coolwarm", as_cmap=True),
+                data=df_subset,
+                legend=False,
+                alpha=0.3,
+            )
+    plt.savefig(save_path)
